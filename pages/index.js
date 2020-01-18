7615dc538750d270198e3757faf8dfd4dd29817e
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-01-18 15:00:29 -0700

    Added all files to repo

--- pages/index.js
+++ pages/index.js
@@ -6,41 +6,18 @@ const Home = () => (
   <div>
     <Head>
       <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <Nav />
 
     <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
+      <img
+        src="https://chimpstatic.com/mcjs-connected/js/users/3814449e93d5456a4bd90d80c/11195c454430253194d5f66fd.js"
+        alt="mail chimp signup"
+      />
     </div>
 
     <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
       .title {
         margin: 0;
         width: 100%;
