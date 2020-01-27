87a34d9d72bc3dee3820d16429f376022d12f906
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-01-26 22:01:31 -0700

    Saved all cahnges, images are still not rendering to the screen.

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,44 @@
 import React from "react";
 import Head from "next/head";
 import Nav from "../components/nav";
+import Newsletter from "../components/newsletter";
+// import BlackSandHiddenSun from "../assets/balck_sand_hidden_sun.jpg";
 
 const Home = () => (
-  <div>
+  <div className="body">
     <Head>
       <title>Home</title>
     </Head>
 
-    <Nav />
-
     <div className="hero">
+      <div className="centered">
+        <Newsletter className="centered_oneline" />
+      </div>
       <img
-        src="https://chimpstatic.com/mcjs-connected/js/users/3814449e93d5456a4bd90d80c/8a6db059c8f47161020716682.js"
-        alt="mail chimp signup"
+        src="/assets/balck_sand_hidden_sun.png"
+        alt="gorgeous view of mountains"
       />
     </div>
-
+    <Nav />
     <style jsx>{`
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
+      .centered {
         display: flex;
-        flex-direction: row;
-        justify-content: space-around;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        flex-direction: column;
       }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
+
+      .centered_oneline {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        flex-direction: column;
       }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
+      .body {
+        height: auto;
+        background-image: url("/assets/balck_sand_hidden_sun.jpg");
       }
     `}</style>
   </div>
